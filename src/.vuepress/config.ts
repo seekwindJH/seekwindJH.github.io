aff8d32b752e1211157c05bca68cc63a2e993670
SeekWind <seekwind@foxmail.com>
2024-02-24 22:00:33 +0800

    update 标题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,8 +5,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "風の跡",
+  description: "愿你所追寻的风，都能如期而至~",
 
   theme,
 
